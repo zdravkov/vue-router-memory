51c21e9ba62f12b5506c06dcd6d4f62f93be5b02
zdravkov <plamen.zdravkov@progress.com>
2022-01-21 09:51:38 +0200

    chore: add project files

---
+++ src/views/About.vue
@@ -0,0 +1,64 @@
+<template>
+<div>
+  <div role="grid" >
+     <div role="presentation"
+      style="padding: 0px;"><div role="presentation">
+        <table role="presentation"><colgroup role="presentation">
+        <col></colgroup>
+        <thead role="presentation" data-keyboardnavheader="true" >
+          <tr>
+            <th aria-sort="none" colspan="1" rowspan="1" role="columnheader"
+           >ProductID</th></tr>
+             </thead>
+        </table>
+      </div>
+    </div>
+             <div role="presentation" >
+               <div role="presentation" >
+                 <div style="position: relative;">
+                   <table role="presentation" tabindex="-1" >
+                     <colgroup role="presentation"><col></colgroup>
+                     <tbody role="presentation" >
+                       <trcomponent v-for="item in result"
+                       :value="item.ProductID" :key="item.ProductID" />
+                     </tbody>
+                   </table>
+                   </div>
+                   </div>
+             </div>
+    </div> 
+</div>
+</template>
+<script>
+import trComponent from './trComponent'
+
+export default {
+    components: {
+      trcomponent: trComponent
+    },
+    beforeDestroy(){
+    },
+    data: function () {
+        return {
+            columns: [
+                { field: 'ProductID'},
+            ],
+             result:this.createRandomData(10000)
+        };
+    },
+    methods: {
+        sortChangeHandler: function(e) {
+            this.sort = e.sort;
+        },
+        createRandomData(count) {
+
+
+            return Array(count).fill({}).map((_, idx) => ({
+                ProductID: idx + 1,
+ 
+            }));
+        }
+    }
+};
+
+</script>
---
+++ src/views/trComponent.vue
@@ -0,0 +1,20 @@
+<template>
+  <tr
+        role="row" 
+        aria-rowindex="2" 
+    data-grid-row-index="-1" 
+    class="k-master-row">
+    <td colspan="1" role="gridcell" data-grid-col-index="0">
+        {{value + "Item"}}
+    </td>
+     </tr>
+</template>
+<script>
+
+export default {
+   props: {
+       value: Number
+   }
+};
+
+</script>
